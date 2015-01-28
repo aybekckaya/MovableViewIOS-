a307b27a83323c2328ec6e4c2bb20c906e03105e
aybekckaya <aybekcankaya@icloud.com>
2015-01-28 16:09:35 +0200

    Initial Commit

---
+++ PanView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PanView
+//
+//  Created by aybek can kaya on 1/28/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PanView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanView
+//
+//  Created by aybek can kaya on 1/28/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
